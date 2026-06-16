8fc068ef5b1fa3ccce0db9e4e5378a83c12f22e4
rafttutoraxis <rafttutoraxis@gmail.com>
2026-06-16 12:40:45 +0530

    Create AdminPage.tsx

---
+++ client/src/AdminPage.tsx
@@ -0,0 +1,10 @@
+import AdminPanel from "./components/AdminPanel";
+
+export default function AdminPage() {
+  return (
+    <AdminPanel
+      lang="en"
+      onForceRefresh={() => {}}
+    />
+  );
+}
