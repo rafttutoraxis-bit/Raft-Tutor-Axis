37a0cc81a4a75f00821d0bea0bf6ad33cc48f272
rafttutoraxis <rafttutoraxis@gmail.com>
2026-06-17 13:49:09 +0530

    Refactor main.tsx to include routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,19 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import App from "./App";
+import AdminPage from "./AdminPage";
+
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
