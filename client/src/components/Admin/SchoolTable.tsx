1a57940117d3254845644545c0979fec8e9dc75e
rafttutoraxis <rafttutoraxis@gmail.com>
2026-06-21 16:18:12 +0530

    Add SchoolTable component with basic structure

--- client/src/components/Admin/SchoolTable.tsx
+++ client/src/components/Admin/SchoolTable.tsx
@@ -1,1 +1,9 @@
+import React from "react";
 
+export default function SchoolTable() {
+  return (
+    <div className="bg-slate-900 rounded-xl p-6 text-white">
+      School Table
+    </div>
+  );
+}
