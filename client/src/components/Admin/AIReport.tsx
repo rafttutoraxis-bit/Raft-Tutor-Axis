cd9fa17d222f02b508dbc1baddf2f17bd7816bc3
rafttutoraxis <rafttutoraxis@gmail.com>
2026-06-21 16:18:54 +0530

    Add AIReport component for Executive Gemini Report

--- client/src/components/Admin/AIReport.tsx
+++ client/src/components/Admin/AIReport.tsx
@@ -1,1 +1,9 @@
+import React from "react";
 
+export default function AIReport() {
+  return (
+    <div className="bg-slate-900 rounded-xl p-6 text-white">
+      Executive Gemini Report
+    </div>
+  );
+}
