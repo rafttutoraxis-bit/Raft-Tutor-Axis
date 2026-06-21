6b0a509b2603c017e788bc5b1340d69d6cbff79d
rafttutoraxis <rafttutoraxis@gmail.com>
2026-06-21 16:17:51 +0530

    Add TeacherTable component with basic layout

--- client/src/components/Admin/TeacherTable.tsx
+++ client/src/components/Admin/TeacherTable.tsx
@@ -1,1 +1,9 @@
+import React from "react";
 
+export default function TeacherTable() {
+  return (
+    <div className="bg-slate-900 rounded-xl p-6 text-white">
+      Teacher Table
+    </div>
+  );
+}
