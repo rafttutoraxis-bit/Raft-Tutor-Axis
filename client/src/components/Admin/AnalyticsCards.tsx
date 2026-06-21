3856e540ff21b72155daa4290d361b8b81c6bf41
rafttutoraxis <rafttutoraxis@gmail.com>
2026-06-21 16:16:29 +0530

    Add AnalyticsCards component for admin dashboard

--- client/src/components/Admin/AnalyticsCards.tsx
+++ client/src/components/Admin/AnalyticsCards.tsx
@@ -1,1 +1,23 @@
+import React from "react";
 
+export default function AnalyticsCards() {
+  return (
+    <div className="grid grid-cols-4 gap-5">
+      <div className="bg-slate-800 rounded-xl p-6 text-white">
+        Total Parents
+      </div>
+
+      <div className="bg-slate-800 rounded-xl p-6 text-white">
+        Teachers
+      </div>
+
+      <div className="bg-slate-800 rounded-xl p-6 text-white">
+        Schools
+      </div>
+
+      <div className="bg-slate-800 rounded-xl p-6 text-white">
+        Logs
+      </div>
+    </div>
+  );
+}
