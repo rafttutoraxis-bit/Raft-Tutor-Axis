b72de158faa6dfe8eecc3a53dc6b97644f8cd67a
rafttutoraxis <rafttutoraxis@gmail.com>
2026-06-21 16:17:28 +0530

    Add ParentTable component with basic structure

--- client/src/components/Admin/ParentTable.tsx
+++ client/src/components/Admin/ParentTable.tsx
@@ -1,1 +1,9 @@
+import React from "react";
 
+export default function ParentTable() {
+  return (
+    <div className="bg-slate-900 rounded-xl p-6 text-white">
+      Parent Table
+    </div>
+  );
+}
